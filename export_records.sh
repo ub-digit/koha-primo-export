6f60a6ee73695077bda14cb28b6305531fe4af80
David Gustafsson <david.gustafsson@ub.gu.se>
2022-04-21 14:08:36 +0200

    Fix missing source of config

--- export_records.sh
+++ export_records.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # This script will export all records in batches
 script_dir="$(dirname "$(readlink -f "$0")")"
+source "$script_dir/config"
+
 IFS=$'\n'
 # TODO: Merge with export_records_p
-for batch in $($koha_shell koha -c "cd \"$koha_path\"; ./misc/record_batches.pl"); do
+for batch in $($koha_shell $koha_instance -c "cd \"$koha_path\"; ./misc/record_batches.pl"); do
     eval $script_dir/export_records_batch.sh $batch
 done
--- export_records_p.sh
+++ export_records_p.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+source "$script_dir/config"
 
 if test "$1" = "--help"
 then
@@ -12,4 +13,4 @@ if [ -n $1 ]; then
     OPTIONS="--gt_biblionumber=$1"
 fi
 
-$koha_shell koha -c "cd \"$koha_path\"; ./misc/record_batches.pl $OPTIONS" | parallel --colsep ' ' -j16 $SCRIPT_DIR/export_records_batch.sh {1} {2} --range-suffix"
+$koha_shell $koha_instance -c "cd \"$koha_path\"; ./misc/record_batches.pl $OPTIONS" | parallel --colsep ' ' -j16 $SCRIPT_DIR/export_records_batch.sh {1} {2} --range-suffix"
